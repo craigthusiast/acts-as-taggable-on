803a307a0817925657a033df0f2cffd1e54610ee
Craig DesBrisay <craig.desbrisay@gmail.com>
2017-03-29 12:55:18 -0400

    add tag_cloud_by_count method to TagsHelper

--- lib/acts_as_taggable_on/tags_helper.rb
+++ lib/acts_as_taggable_on/tags_helper.rb
@@ -4,6 +4,17 @@ module ActsAsTaggableOn
     def tag_cloud(tags, classes)
       return [] if tags.empty?
 
+      max_count = tags.sort_by(&:taggings_count).last.taggings_count.to_f
+
+      tags.each do |tag|
+        index = ((tag.taggings_count / max_count) * (classes.size - 1))
+        yield tag, classes[index.nan? ? 0 : index.round]
+      end
+    end
+    
+    def tag_cloud_by_count(tags, classes)
+      return [] if tags.empty?
+
       max_count = tags.sort_by(&:count).last.count.to_f
 
       tags.each do |tag|
